d31cf08e0abe242c8e514fb62f61e70a72683d85
Sagar-turtu <sagar@turtu.in>
2026-04-14 15:27:27 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -238,6 +238,7 @@ function App() {
                     loop
                     muted
                     playsInline
+                    controls
                   />
                 </div>
               </MotionArticle>
